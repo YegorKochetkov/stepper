af64ed98475098fd1f77b07b797ddb06d48322b1
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-08-31 01:56:44 +0300

    feat[components]: add Stepper

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import "./App.css";
+import React from "react";
+
+import Stepper from "./components/Stepper/Stepper";
 
 function App() {
   return (
-    <main className='max-w-7xl mx-auto my-0 p-8 text-center'>
-      <button>Hello!</button>
+    <main className='max-w-7xl p-8'>
+      <Stepper />
     </main>
   );
 }
---
+++ src/components/Stepper/Stepper.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Stepper = () => {
+  return <button>Next</button>;
+};
+
+export default Stepper;
