e985ba73cf73488a08b76cd68b731b0a889edf86
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-08-31 01:44:37 +0300

    build: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <button>Hello!</button>;
+  return (
+    <main className='max-w-7xl mx-auto my-0 p-8 text-center'>
+      <button>Hello!</button>
+    </main>
+  );
 }
 
 export default App;
