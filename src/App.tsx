1d633aece9340973ec4b874387419cf161596341
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-08-31 12:33:30 +0300

    feat: implement Stepper logic & styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import Stepper from "./components/Stepper/Stepper";
 
+const stepsFromBackend = ["Customer info", "Shipping info", "Payment"];
+
 function App() {
   return (
-    <main className='max-w-7xl p-8'>
-      <Stepper />
+    <main className='max-w-7xl p-8 text-center'>
+      <Stepper steps={stepsFromBackend} />
     </main>
   );
 }
--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -1,7 +1,40 @@
 import React from "react";
 
-function Stepper() {
-  return <button>Next</button>;
+function Stepper({ steps }: { steps: string[] }) {
+  const [currentStep, setCurrentStep] = React.useState(1);
+  const isCompleted = currentStep > steps.length;
+
+  return (
+    <React.Fragment>
+      <div className='flex justify-between mb-8'>
+        {steps.map((step, i) => (
+          <div
+            key={i}
+            className={
+              "step-item" +
+              " " +
+              (currentStep === i + 1 ? "active" : "") +
+              (currentStep > i + 1 ? "complete" : "")
+            }
+          >
+            <div className='step-index'>
+              {currentStep > i + 1 ? "✔️" : i + 1}
+            </div>
+            <p className='capitalize'>{step}</p>
+          </div>
+        ))}
+      </div>
+
+      <button
+        type='button'
+        className='button'
+        onClick={() => setCurrentStep((prev) => prev + 1)}
+        disabled={isCompleted}
+      >
+        {isCompleted ? "Completed" : "Next"}
+      </button>
+    </React.Fragment>
+  );
 }
 
 export default Stepper;
